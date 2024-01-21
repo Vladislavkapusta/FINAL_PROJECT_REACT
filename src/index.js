f779293b5df4edb9fb5fe8ecc32e473ef85f4afe
Vladislavkapusta <KapustinVi21@st.ithub.ru>
2024-01-21 16:09:28 +0300

    Backend now live! (categories images visual error comes from hosting site)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-export const BASE_URL = 'http://localhost:3333'
+
+export const BASE_URL = 'https://backend-garden.onrender.com'
+// export const BASE_URL = 'http://localhost:3333'
 // export const BASE_URL = 'https://project-fe-vxeu.onrender.com'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
